77f6eaedf52d9d9399212ba6dfe6b79529e28342
jsconan <jsconan@gmail.com>
2022-08-19 21:48:12 +0200

    refactor: use simpler name for the parameters of the straight barrier element

--- src/tiles/StraightBarrier.svelte
+++ src/tiles/StraightBarrier.svelte
@@ -2,22 +2,22 @@
     // Licensed under GNU Public License version 3
     // Copyright (c) 2022 Jean-Sébastien CONAN
 
-    export let barrierChunks;
-    export let barrierWidth;
-    export let barrierLength;
+    export let chunks;
+    export let width;
+    export let length;
     export let x = 0;
     export let y = 0;
     export let shift = 0;
     export let vertical = false;
 
     const colors = ['even', 'odd'];
-    const halfBarrier = barrierWidth / 2;
+    const halfBarrier = width / 2;
 
-    function* chunks() {
-        for (let i = 0; i < barrierChunks; i++) {
+    function* segments() {
+        for (let i = 0; i < chunks; i++) {
             const color = colors[(i + shift) % 2];
-            const a = i * barrierLength;
-            const b = a + barrierLength;
+            const a = i * length;
+            const b = a + length;
 
             const x1 = vertical ? x + halfBarrier : x + a;
             const y1 = vertical ? y + a : y + halfBarrier;
@@ -30,7 +30,7 @@
 </script>
 
 <g class="barrier straight-barrier">
-    {#each [...chunks()] as { color, x1, y1, x2, y2 }}
-        <line class="barrier-chunk {color}" {x1} {y1} {x2} {y2} stroke-width={barrierWidth} />
+    {#each [...segments()] as { color, x1, y1, x2, y2 }}
+        <line class="barrier-chunk {color}" {x1} {y1} {x2} {y2} stroke-width={width} />
     {/each}
 </g>
--- src/tiles/StraightTile.svelte
+++ src/tiles/StraightTile.svelte
@@ -2,6 +2,7 @@
     // Licensed under GNU Public License version 3
     // Copyright (c) 2022 Jean-Sébastien CONAN
 
+    import { getStraightBarrierChunks } from '../helpers/track.js';
     import StraightBarrier from './StraightBarrier.svelte';
 
     export let barrierChunks;
@@ -17,18 +18,19 @@
     const height = tileWidth;
     const barrierLength = tileLength / barrierChunks;
     const tilePadding = (tileLength - tileWidth) / 2;
-    const allChunks = barrierChunks * tileRatio;
+    const laneWidth = tileWidth - barrierWidth;
+    const sideChunks = getStraightBarrierChunks(barrierChunks, tileRatio);
 
     const x = tileX;
     const y = tileY + tilePadding;
     const y1 = y;
-    const y2 = y + tileWidth - barrierWidth;
+    const y2 = y + laneWidth;
     const cx = x + width / 2;
     const cy = y + height / 2;
 </script>
 
 <g class="tile straight-tile" transform="rotate({tileAngle} {cx} {cy})">
     <rect class="ground" {x} {y} {width} {height} />
-    <StraightBarrier barrierChunks={allChunks} {barrierWidth} {barrierLength} {x} y={y1} shift={0} />
-    <StraightBarrier barrierChunks={allChunks} {barrierWidth} {barrierLength} {x} y={y2} shift={1} />
+    <StraightBarrier chunks={sideChunks} width={barrierWidth} length={barrierLength} {x} y={y1} shift={0} />
+    <StraightBarrier chunks={sideChunks} width={barrierWidth} length={barrierLength} {x} y={y2} shift={1} />
 </g>
