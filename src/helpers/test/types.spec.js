65aa2b3aacbc84ca4ec7761957900999e7e3703f
jsconan <jsconan@gmail.com>
2022-08-28 12:15:06 +0200

    feat: add helpers and constants for the types of tiles

---
+++ src/helpers/test/types.spec.js
@@ -0,0 +1,59 @@
+/**
+ * RC Tracks Editor
+ * Copyright (c) 2022 Jean-Sébastien CONAN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+    CURVED_TILE_ENLARGED_TYPE,
+    CURVED_TILE_TYPE,
+    DEFAULT_TILE_TYPE,
+    isDirectionValid,
+    isTypeValid,
+    STRAIGHT_TILE_TYPE,
+    TILE_DIRECTION_LEFT,
+    TILE_DIRECTION_RIGHT
+} from '../types.js';
+
+describe('isDirectionValid', () => {
+    it('is a function', () => {
+        expect(isDirectionValid).toEqual(expect.any(Function));
+    });
+
+    it.each([TILE_DIRECTION_RIGHT, TILE_DIRECTION_LEFT])('tells the direction "%s" is valid', direction => {
+        expect(isDirectionValid(direction)).toBeTruthy();
+    });
+
+    it.each([-1, 2, 'left', 'right'])('tells the direction "%s" is not valid', direction => {
+        expect(isDirectionValid(direction)).toBeFalsy();
+    });
+});
+
+describe('isTypeValid', () => {
+    it('is a function', () => {
+        expect(isTypeValid).toEqual(expect.any(Function));
+    });
+
+    it.each([STRAIGHT_TILE_TYPE, CURVED_TILE_TYPE, CURVED_TILE_ENLARGED_TYPE])(
+        'tells the type "%s" is valid',
+        direction => {
+            expect(isTypeValid(direction)).toBeTruthy();
+        }
+    );
+
+    it.each([DEFAULT_TILE_TYPE, 'left', 'right'])('tells the type "%s" is not valid', direction => {
+        expect(isTypeValid(direction)).toBeFalsy();
+    });
+});
