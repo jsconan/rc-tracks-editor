422367fc6e32fc877bcff7af0202cab932260c48
jsconan <jsconan@gmail.com>
2022-09-03 19:15:56 +0200

    feat: add a component for rendering the track

---
+++ src/components/Track.svelte
@@ -0,0 +1,24 @@
+<script>
+    // Licensed under GNU Public License version 3
+    // Copyright (c) 2022 Jean-Sébastien CONAN
+
+    import Sketch from '../elements/Sketch.svelte';
+    import { TrackModel } from '../models/TrackModel';
+
+    export let model;
+    export let angle = 0;
+    export let x = 0;
+    export let y = 0;
+
+    if (!(model instanceof TrackModel)) {
+        throw new TypeError('The model must be an instance of TrackModel!');
+    }
+
+    $: track = $model.build(0, 0, angle);
+</script>
+
+<Sketch {x} {y} viewX={track.x} viewY={track.y} viewWidth={track.width} viewHeight={track.height}>
+    {#each track.tiles as { id, x, y, angle, model, component }}
+        <svelte:component this={component} {model} {angle} {x} {y} {id} />
+    {/each}
+</Sketch>
