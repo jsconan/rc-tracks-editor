08e9f3d1f5d5530550961866c5621ea735e00d55
jsconan <jsconan@gmail.com>
2022-09-03 21:25:38 +0200

    feat: use the track component

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,9 @@
     // Licensed under GNU Public License version 3
     // Copyright (c) 2022 Jean-Sébastien CONAN
 
-    import OutlineFilter from './filters/OutlineFilter.svelte';
-
     import config from './config.js';
-    import ControlPoints from './components/ControlPoints.svelte';
     import Sketch from './elements/Sketch.svelte';
+    import Track from './components/Track.svelte';
     import { TileSpecifications } from './models/TileSpecifications.js';
     import { TrackModel } from './models/TrackModel.js';
     import {
@@ -22,44 +20,38 @@
     const barrierWidth = config.barrierWidth;
     const laneWidth = config.laneWidth;
 
+    const angle = -90;
     const specs = new TileSpecifications(laneWidth, barrierWidth, barrierChunks);
+    const model = new TrackModel(specs);
 
-    const trackModel = new TrackModel(specs);
+    model.appendTile(STRAIGHT_TILE_TYPE, TILE_DIRECTION_RIGHT);
+    model.appendTile(STRAIGHT_TILE_TYPE, TILE_DIRECTION_RIGHT);
+    model.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
+    model.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
+    model.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
+    model.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT, 3);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT, 3);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT, 3);
+    model.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
+    model.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
+    model.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
 
-    trackModel.appendTile(STRAIGHT_TILE_TYPE, TILE_DIRECTION_RIGHT);
-    trackModel.appendTile(STRAIGHT_TILE_TYPE, TILE_DIRECTION_RIGHT);
-    trackModel.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
-    trackModel.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
-    trackModel.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
-    trackModel.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT, 3);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT, 3);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_RIGHT, 3);
-    trackModel.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
-    trackModel.appendTile(CURVED_TILE_ENLARGED_TYPE, TILE_DIRECTION_LEFT);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 4);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
-    trackModel.appendTile(CURVED_TILE_TYPE, TILE_DIRECTION_LEFT, 2);
-
-    // trackModel.import(data.square);
-
-    const track = trackModel.build(0, 0, -90);
-    console.log(JSON.stringify(trackModel.export()));
+    console.log(JSON.stringify(model.export()));
+    // model.import(data.square)
 </script>
 
-<Sketch x={track.x} y={track.y} viewWidth={track.width} viewHeight={track.height} width="100%" height="100%">
-    <OutlineFilter R={0.2} G={0.9} B={0.4} A={0.9} width={6} slot="defs" />
-    {#each track.tiles as { id, x, y, angle, model, component }}
-        <svelte:component this={component} {model} {angle} {x} {y} {id} />
-        <!-- <ControlPoints {model} {angle} {x} {y} /> -->
-    {/each}
+<Sketch width="100%" height="100%">
+    <Track {model} {angle} />
 </Sketch>
