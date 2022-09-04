502feaa03bea276e3878d216f88abd672f06c7e3
jsconan <jsconan@gmail.com>
2022-09-04 23:14:44 +0200

    feat: add a component for representing the control points of a track tile

---
+++ src/track/components/ControlPoints.svelte
@@ -0,0 +1,25 @@
+<script>
+    // Licensed under GNU Public License version 3
+    // Copyright (c) 2022 Jean-Sébastien CONAN
+
+    import { TileModel } from '../models';
+
+    export let model;
+    export let angle = 0;
+    export let x = 0;
+    export let y = 0;
+    export let r = 5;
+
+    if (!(model instanceof TileModel)) {
+        throw new TypeError('The model must be an instance of TileModel!');
+    }
+
+    const output = model.getOutputCoord(x, y, angle);
+    const center = model.getCenterCoord(x, y, angle);
+</script>
+
+<g class="control">
+    <circle class="point" cx={x} cy={y} {r} />
+    <circle class="point" cx={center.x} cy={center.y} {r} />
+    <circle class="point" cx={output.x} cy={output.y} {r} />
+</g>
