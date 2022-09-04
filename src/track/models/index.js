996d0bcdc44a0b261a18752961d92017d83515a2
jsconan <jsconan@gmail.com>
2022-09-04 23:01:12 +0200

    feat: add a model referencing the track tiles from one place

--- src/track/models/index.js
+++ src/track/models/index.js
@@ -20,4 +20,5 @@ export { CurvedTileEnlargedModel } from './CurvedTileEnlargedModel.js';
 export { CurvedTileModel } from './CurvedTileModel.js';
 export { StraightTileModel } from './StraightTileModel.js';
 export { TileModel } from './TileModel.js';
+export { TileReferenceModel } from './TileReferenceModel.js';
 export { TileSpecifications } from './TileSpecifications.js';
