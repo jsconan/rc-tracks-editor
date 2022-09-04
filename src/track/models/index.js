677ff5d45d1cb74eb88172190fc2d475d73cef7e
jsconan <jsconan@gmail.com>
2022-09-04 22:17:44 +0200

    feat: add a common implementation for the tile models

--- src/track/models/index.js
+++ src/track/models/index.js
@@ -16,4 +16,5 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+export { TileModel } from './TileModel.js';
 export { TileSpecifications } from './TileSpecifications.js';
