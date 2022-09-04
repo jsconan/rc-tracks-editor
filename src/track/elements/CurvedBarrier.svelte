4ffdb9691ada641583cd5941063a301ff125cc58
jsconan <jsconan@gmail.com>
2022-09-04 22:44:27 +0200

    feat: add a component representing a curved barrier

---
+++ src/track/elements/CurvedBarrier.svelte
@@ -0,0 +1,34 @@
+<script>
+    // Licensed under GNU Public License version 3
+    // Copyright (c) 2022 Jean-Sébastien CONAN
+
+    import { alternateBarrierColor } from '../helpers';
+    import CurvedElement from './CurvedElement.svelte';
+
+    export let chunks;
+    export let width;
+    export let angle;
+    export let radius;
+    export let left = 0;
+    export let top = 0;
+    export let shift = 0;
+
+    const chunkAngle = angle / chunks;
+    const cx = left - radius;
+    const cy = top;
+
+    function* segments() {
+        for (let i = 0; i < chunks; i++) {
+            const color = alternateBarrierColor(i + shift);
+            const start = chunkAngle * i;
+
+            yield { color, start, i };
+        }
+    }
+</script>
+
+<g class="barrier curved-barrier">
+    {#each [...segments()] as { color, start, i } (i)}
+        <CurvedElement class="barrier-chunk {color}" {cx} {cy} {width} {radius} angle={chunkAngle} {start} />
+    {/each}
+</g>
--- src/track/elements/index.js
+++ src/track/elements/index.js
@@ -16,6 +16,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+export { default as CurvedBarrier } from './CurvedBarrier.svelte';
 export { default as CurvedElement } from './CurvedElement.svelte';
 export { default as StraightBarrier } from './StraightBarrier.svelte';
 export { default as StraightElement } from './StraightElement.svelte';
