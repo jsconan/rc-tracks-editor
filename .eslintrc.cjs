8035df8ad034df73792149a6a9c26041208c6205
jsconan <jsconan@gmail.com>
2022-10-13 21:02:53 +0200

    chore: make the toolchain support modern features

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,7 +23,7 @@ module.exports = {
     overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
     parserOptions: {
         sourceType: 'module',
-        ecmaVersion: 2020
+        ecmaVersion: 'latest'
     },
     env: {
         browser: true,
