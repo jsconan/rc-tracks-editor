3385fd401655ffdf91595e69a1116146e4a7d53e
jsconan <jsconan@gmail.com>
2022-10-16 23:59:12 +0200

    feat: add a component for showing the used tiles

---
+++ src/track/components/TrackStats.svelte
@@ -0,0 +1,55 @@
+<script>
+    // Licensed under GNU Public License version 3
+    // Copyright (c) 2022 Jean-Sébastien CONAN
+
+    import { Sketch } from '../elements';
+    import { Tile } from '../../tile/components';
+    import { buildList } from '../helpers';
+    import { TrackModel } from '../models';
+
+    export let track;
+    export let x = 0;
+    export let y = 0;
+    export let width = void 0;
+    export let height = void 0;
+
+    TrackModel.validateInstance(track);
+
+    const counterStore = track.counterStore;
+    const modelsStore = track.modelsStore;
+    const specs = track.specs;
+
+    const getTextX = (x, rect) => x + rect.x + rect.width / 2;
+    const getTextY = (y, rect) => y + rect.y + rect.height / 2;
+
+    $: models = buildList($modelsStore, {
+        tileAngle: -0,
+        centered: true,
+        aligned: true,
+        vertical: false,
+        hPadding: specs.padding,
+        vPadding: specs.padding
+    });
+</script>
+
+<Sketch {x} {y} {width} {height} viewX={models.x} viewY={models.y} viewWidth={models.width} viewHeight={models.height}>
+    {#each models.tiles as { id, type, direction, ratio, x, y, angle, rect }, i (id)}
+        <Tile {type} {direction} {ratio} {angle} {x} {y} />
+        <text
+            x={getTextX(x, rect)}
+            y={getTextY(y, rect)}
+            text-anchor="middle"
+            dominant-baseline="middle"
+            style="--font-size: {specs.width / 3}px"
+        >
+            {$counterStore[i].count}
+        </text>
+    {/each}
+</Sketch>
+
+<style>
+    text {
+        font: var(--font-size) sans-serif;
+        fill: var(--color-fg);
+    }
+</style>
--- src/track/components/index.js
+++ src/track/components/index.js
@@ -17,3 +17,4 @@
  */
 
 export { default as SimpleTrack } from './SimpleTrack.svelte';
+export { default as TrackStats } from './TrackStats.svelte';
